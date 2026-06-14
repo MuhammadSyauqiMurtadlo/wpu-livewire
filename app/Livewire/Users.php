8b5c67ed353cfe7d9abd2d524371c853319b3ad3
Syauqi <sauki084@gmail.com>
2026-06-14 12:22:15 +0700

    WPU Livewire: Data Binding & Reset

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -8,15 +8,22 @@ use Livewire\Component;
 
 class Users extends Component
 {
-    // public $title = 'Users Page Livewire';
-    public function createUser()
+    public $name;
+
+    public $email;
+
+    public $password;
+
+    public function createNewUser()
     {
         // User::factory()->create();
         User::create([
-            'name' => 'Muhammad Syauqi Murtadlo',
-            'email' => 'syauqi@example.com',
-            'password' => Hash::make('password'),
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
         ]);
+
+        $this->reset();
     }
 
     public function render()
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,10 +1,45 @@
 <div class="w-1/2 m-auto my-10">
-    <h1 class="text-2xl font-semibold">{{ $title }}</h1>
-    <p class="text-gray-500">Total Users: {{ $users->count() }}</p>
-    <button wire:click="createUser" type="button"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2 cursor-pointer">
-        Create
-    </button>
+    <div class="py-2">
+        <div class="mx-auto">
+            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create User</h2>
+        </div>
+
+        <div class="mt-10">
+            <form wire:submit.prevent="createNewUser" class="space-y-6">
+                <div>
+                    <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+                    <div class="mt-2">
+                        <input wire:model="name" id="name" type="text" name="name" autocomplete="name"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                <div>
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                    <div class="mt-2">
+                        <input wire:model="email" id="email" type="email" name="email" required
+                            autocomplete="email"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+                    </div>
+                    <div class="mt-2">
+                        <input wire:model="password" id="password" type="password" name="password" required
+                            autocomplete="current-password"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                <div>
+                    <button
+                        class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign
+                        in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
 
     <hr class="border-1 my-5">
     <h2 class="text-2xl font-semibold mb-2">Users List</h2>
